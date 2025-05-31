d55dc2a970d391013cb6b0cc21c2267a652a95e3
Numpkens <numpkins188@gmail.com>
2025-05-31 09:57:04 +0800

    "Add files and images"

---
+++ script.js
@@ -0,0 +1,7 @@
+/* SIMPLE: Build a web page with two dice images and a "Roll" button.
+    - Use HTML to lay out the dice and the button.
+    - Use CSS to style the layout and add hover/focus effects.
+    - Use JS to generate two random numbers and update the dice images accordingly. */
+
+
+
